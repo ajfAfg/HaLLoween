f25bc031f0bc3315fe794b19636061f9ff27e7dd
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-08-29 07:16:39 +0900

    Update with the Halloween module

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,12 @@
 ExUnit.start()
+
+# Find an appropriate seed as follows.
+# 1..10
+# |> Enum.to_list()
+# |> Enum.map(fn n ->
+#   :rand.seed(:exsss, n)
+#   {Enum.random(0..100), Enum.random(0..100)}
+# end)
+#
+# First and second random number are 16 and 88, respectively.
+ExUnit.configure(seed: 30)
