70150674d63517292f75fc4cc8084ae4d2bc86b0
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-08-29 07:13:07 +0900

    Update

--- lib/halloween.ex
+++ lib/halloween.ex
@@ -1,18 +1,33 @@
 defmodule Halloween do
+  @low_border 20
+  @high_border 100
+
   @moduledoc """
   Documentation for `Halloween`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Halloween.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def to_halloween(text, halloween) do
+    text
+    |> String.split()
+    |> Enum.map(&halloween_with_halloween(&1, halloween))
+    |> Enum.join(" ")
   end
+
+  def halloween_with_halloween(str, halloween) do
+    if rand() <= halloween do
+      halloween |> to_halloween_level() |> halloween()
+    else
+      str
+    end
+  end
+
+  def to_halloween_level(n) when n <= @low_border, do: :low
+  def to_halloween_level(n) when @high_border <= n, do: :high
+  def to_halloween_level(_), do: :midium
+
+  def halloween(:low), do: "HALLOWEEN"
+  def halloween(:midium), do: "HALLOWEEN!"
+  def halloween(:high), do: "HALLOWEEN..."
+
+  defp rand(), do: Enum.random(0..100)
 end
